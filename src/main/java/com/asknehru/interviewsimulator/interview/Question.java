d27a68b818aadeb987b505bdf44c91e4ea01022b
jawaharlalnehru1988 <nehru00123@gmail.com>
2026-06-29 04:46:17 +0530

    feat: add EASY option to Difficulty enum in Question model

--- src/main/java/com/asknehru/interviewsimulator/interview/Question.java
+++ src/main/java/com/asknehru/interviewsimulator/interview/Question.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Question {
 
     public enum Difficulty {
-        SUPER_EASY, COMPLEXITY, MEDIUM, HARD, HARDER
+        SUPER_EASY, COMPLEXITY, EASY, MEDIUM, HARD, HARDER
     }
 
     @Id
